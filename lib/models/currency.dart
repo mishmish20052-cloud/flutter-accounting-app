c283025256684b7d563e504a6f79a78c5134a8c7
mishmish20052-cloud <mishmish20052@gmail.com>
2026-06-01 19:34:12 +0300

    Create currency.dart

---
+++ lib/models/currency.dart
@@ -0,0 +1,8 @@
+class Currency {
+  final String code;
+  final String name;
+  final double rateToUsd;
+  final String symbol;
+
+  Currency({required this.code, required this.name, required this.rateToUsd, required this.symbol});
+}
